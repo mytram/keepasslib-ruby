7c228e417063367b339809965d0814d5636f4d47
Michael Nowak <thexsystem@gmail.com>
2013-09-01 22:09:27 +0200

    fixed some typos; improved load path stuff

--- test/run.rb
+++ test/run.rb
@@ -1,4 +1,6 @@
-reqxuire 'keepass/KdbReaderFactory'
+$LOAD_PATH.insert(0, File.expand_path('../../lib', __FILE__))
+
+require 'keepass/KdbReaderFactory'
 require 'keepass/KdbPassword'
 
 factory = KeePassLib::KdbReaderFactory.new
@@ -7,5 +9,4 @@ pass = KeePassLib::KdbPassword.new('test')
 
 #reader_v3 = factory.load("test/sample_v3.kdb", pass )
 
-reader_v4 = factory.load("test/sample_v4.kdbx", pass)
-
+reader_v4 = factory.load(File.expand_path("../sample_v4.kdbx", __FILE__), pass)
